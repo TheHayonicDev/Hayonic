08c910aec0d06cc4d2b85720f42e254608913d90
TheHayonicDev <108293666+TheHayonicDev@users.noreply.github.com>
2023-02-15 13:51:11 +0000

    Update 15.02.23

---
+++ Assets/Settings.lua
@@ -0,0 +1,15 @@
+return {
+    ["Discord"] = {
+        ["Invite"] = "FybnZuwKTS"
+    },
+
+    ["Key"] = {
+        ["GetLink"] = "https://linktr.ee/TheHayonicDev"
+    },
+
+    ["Whitelist"] = {
+        ["BannedHWIDs"] = {
+            "63955025-F666-435F-9293-1C97E34FBF8D"
+        }
+    }
+}
